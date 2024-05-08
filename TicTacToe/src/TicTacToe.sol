67a7d3e37d584fde51ee20e23ec849cc8c6f39cf
charlielukman <lukman.charlie@gmail.com>
2024-05-08 17:31:49 +0700

    Add TicTacToe win condition check in isWinning function

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -11,6 +11,25 @@ contract TicTacToe {
     */
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
+        // Check rows and columns
+        for (uint8 i = 0; i < 3; i++) {
+            if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+                return true;
+            }
+            if (board[0][i] != 0 && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (board[0][0] != 0 && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+            return true;
+        }
+        if (board[0][2] != 0 && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+            return true;
+        }
+
+        // No win condition found
+        return false;
     }
 }
