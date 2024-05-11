327af5d4a24014eeafc4e506a52e41425f22c691
charlielukman <lukman.charlie@gmail.com>
2024-05-11 17:10:49 +0700

    Implement deposit and withdrawal functionality in BasicBank and BasicBankV2 contracts

--- BasicBank/src/BasicBank.sol
+++ BasicBank/src/BasicBank.sol
@@ -2,10 +2,17 @@
 pragma solidity ^0.8.13;
 
 contract BasicBank {
+    mapping(address => uint256) public balances;
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    function addEther() external payable {
+        balances[msg.sender] += msg.value;
+    }
 
     /// @notice used to withdraw ether from the contract (No restriction on withdrawals)
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
 }
